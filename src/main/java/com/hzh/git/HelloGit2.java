2e5c52c388270d7606bf8674adb0d048f38c31d9
ZhaoHui-Hou <hou756444@139.com>
2021-04-18 18:59:36 +0800

    pom

---
+++ src/main/java/com/hzh/git/HelloGit2.java
@@ -0,0 +1,13 @@
+package com.hzh.git;
+
+/**@Description: 
+ * @author: hzh
+ * @date: 2021年4月18日下午6:39:17 
+ */
+
+public class HelloGit2 {
+
+	public static void main(String[] args) {
+		System.out.println("Hello Git!!");
+	}
+}
