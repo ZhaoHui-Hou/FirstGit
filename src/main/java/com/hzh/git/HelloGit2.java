83b9b435ed2d020214504414297d198111403b4e
ZhaoHui-Hou <hou756444@139.com>
2021-04-18 19:08:40 +0800

    创建分支

--- src/main/java/com/hzh/git/HelloGit2.java
+++ src/main/java/com/hzh/git/HelloGit2.java
@@ -10,6 +10,6 @@ public class HelloGit2 {
 	public static void main(String[] args) {
 		System.out.println("Hello Git!!");
 		System.out.println("你好！！！！！");
-		
+		System.out.println("创建分支");
 	}
 }
